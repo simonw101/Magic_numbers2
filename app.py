b3608249766f4e2846b7f0c16bf4767fc5a54f94
simonw101 <simonhanwilson@hotmail.co.uk>
2021-01-21 12:22:58 +0000

    random added

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import random
 
-magic_numbers = [random.randint(1, 10), random.randint(1, 10)]
+magic_numbers = [random.randint(0, 10), random.randint(0, 10)]
 
-user_number = 88
+chances = 3
 
-if user_number in magic_numbers:
-    print("you got it right")
+for i in range(chances):  # range(chances) is [0,1,2]
 
-if user_number not in magic_numbers:
-    print("You got it wrong")
+    print("This is attempt {}".format(i+1))
+
+    user_number = int(input("Please pick a number between 0 and 9: "))
+
+    if user_number in magic_numbers:
+        print("you got it right")
+
+    if user_number not in magic_numbers:
+        print("You got it wrong")
+
+
+
