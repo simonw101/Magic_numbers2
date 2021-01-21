a21c4c9b0ccc0bfde50a4dd4ea1e861a12f90f42
simonw101 <simonhanwilson@hotmail.co.uk>
2021-01-21 14:31:33 +0000

    complete practise program

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 import random
 
-magic_numbers = [random.randint(0, 10), random.randint(0, 10)]
+magic_numbers = [random.randint(0, 9), random.randint(0, 9)]
 
-chances = 3
 
-for i in range(chances):  # range(chances) is [0,1,2]
-
-    print("This is attempt {}".format(i+1))
-
+def ask_user_and_check_number():
     user_number = int(input("Please pick a number between 0 and 9: "))
 
     if user_number in magic_numbers:
-        print("you got it right")
+        return "you got it right"
 
     if user_number not in magic_numbers:
-        print("You got it wrong")
+        return "You got it wrong"
 
 
+def run_programme_x_times(num):
+    for i in range(num):  # range(chances) is [0,1,2]
 
+        print("This is attempt {}".format(i+1))
+
+        message = ask_user_and_check_number()
+
+        print(message)
+
+
+user_attempts = int(input("How many times do you want to run program: "))
+run_programme_x_times(user_attempts)
